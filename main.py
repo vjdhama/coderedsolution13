27b29542510bc27c217d9ba8e1824d57f9a1a87d
vjdhama <vjdhama26@gmail.com>
2013-02-05 11:05:11 -0800

    added signup page

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2,os
+import jinja2
+
+
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+        
+    def render_str(self, template, **params):
+        t = jinja_environment.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
+    def get(self):
+        #template = jinja_environment.get_template('base.html')
+        #self.response.out.write(template.render())
+        self.render("base.html")
+        #self.response.out.write('dawddwad')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
